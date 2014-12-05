c6213b61cc9dea8076f091d6e0a6ddbf275a4af3
Pedro Belo <pedro@heroku.com>
2014-12-05 14:07:53 -0800

    definition takes attributes on initializer

--- lib/sinatra/schema/definition.rb
+++ lib/sinatra/schema/definition.rb
@@ -2,6 +2,13 @@ module Sinatra
   module Schema
     class Definition
       attr_accessor :description, :example, :format, :type
+
+      def initialize(options)
+        @id          = options[:id]
+        @description = options[:description]
+        @example     = options[:example]
+        @type        = options[:type]
+      end
     end
   end
 end
