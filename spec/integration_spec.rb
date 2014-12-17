84bc285b58f6765f40cb5dd7f73470d25790fc06
Pedro Belo <pedro@heroku.com>
2014-12-17 15:09:45 -0800

    separate tests

--- spec/integration_spec.rb
+++ spec/integration_spec.rb
@@ -1,24 +1,55 @@
 require "spec_helper"
 
 describe Sinatra::Schema do
-  it "still works as a regular sinatra app" do
-    get "/regular"
-    assert_equal 200, last_response.status
-    assert_equal "hi", last_response.body
+  describe "resource access" do
+    it "still works as a regular sinatra app" do
+      get "/regular"
+      assert_equal 200, last_response.status
+      assert_equal "hi", last_response.body
+    end
+
+    it "support resource gets" do
+      get "/accounts"
+      assert_equal 200, last_response.status
+      assert_equal({ "email" => "foo@bar.com" },
+        MultiJson.decode(last_response.body))
+    end
+
+    it "support resource posts" do
+      post "/accounts", email: "omg"
+      assert_equal 200, last_response.status
+      assert_equal({ "email" => "omg" },
+        MultiJson.decode(last_response.body))
+    end
   end
 
-  it "support resource gets" do
-    get "/accounts"
-    assert_equal 200, last_response.status
-    assert_equal({ "email" => "foo@bar.com" },
-      MultiJson.decode(last_response.body))
-  end
+  describe "GET /schema" do
+    before do
+      get "/schema"
+      @schema = MultiJson.decode(last_response.body)
+    end
 
-  it "support resource posts" do
-    post "/accounts", email: "omg"
-    assert_equal 200, last_response.status
-    assert_equal({ "email" => "omg" },
-      MultiJson.decode(last_response.body))
+    it "renders a 200" do
+      assert_equal 200, last_response.status
+    end
+
+    it "sets the appropriate content-type" do
+      assert_equal "application/schema+json",
+        last_response.headers["Content-Type"]
+    end
+
+    it "sets $schema to hyper-schema" do
+      assert_equal "http://json-schema.org/draft-04/hyper-schema",
+        @schema["$schema"]
+    end
+
+    it "generates definitions" do
+      assert_equal Hash, @schema["definitions"].class
+      assert_equal Hash, @schema["definitions"]["account"].class
+      assert_equal "Account", @schema["definitions"]["account"]["title"]
+      assert_equal "An account represents an individual signed up to use the service",
+        @schema["definitions"]["account"]["description"]
+    end
   end
 
   describe "error handling" do
--- spec/json_schema_spec.rb
+++ spec/json_schema_spec.rb
@@ -1,30 +1,18 @@
 require "spec_helper"
 
-describe "JSON Schema" do
-  before do
-    get "/schema"
-    @schema = MultiJson.decode(last_response.body)
-  end
+describe Sinatra::Schema::JsonSchema do
+  let(:json_schema) { described_class.new(root) }
+  let(:root)        { Sinatra::Schema::Root.instance }
+  let(:schema)      { json_schema.dump }
 
-  it "renders a 200" do
-    assert_equal 200, last_response.status
-  end
+  describe "root level" do
+    it "sets $schema" do
+      assert_equal "http://json-schema.org/draft-04/hyper-schema",
+        schema["$schema"]
+    end
 
-  it "sets the appropriate content-type" do
-    assert_equal "application/schema+json",
-      last_response.headers["Content-Type"]
-  end
-
-  it "sets $schema to hyper-schema" do
-    assert_equal "http://json-schema.org/draft-04/hyper-schema",
-      @schema["$schema"]
-  end
-
-  it "generates definitions" do
-    assert_equal Hash, @schema["definitions"].class
-    assert_equal Hash, @schema["definitions"]["account"].class
-    assert_equal "Account", @schema["definitions"]["account"]["title"]
-    assert_equal "An account represents an individual signed up to use the service",
-      @schema["definitions"]["account"]["description"]
+    it "adds a definition hash" do
+      assert_instance_of Hash, schema["definitions"]
+    end
   end
 end
