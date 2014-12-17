0850ed63eb71070b1f1abb1deea2a32b2b604cb8
Pedro Belo <pedro@heroku.com>
2014-12-17 15:06:07 -0800

    extract class

--- lib/sinatra/schema/json_schema.rb
+++ lib/sinatra/schema/json_schema.rb
@@ -1,7 +1,17 @@
 module Sinatra
   module Schema
     class JsonSchema
+      attr_accessor :root
+
       def self.dump(root)
+        new(root).dump
+      end
+
+      def initialize(root)
+        @root = root
+      end
+
+      def dump
         {
           "$schema" => "http://json-schema.org/draft-04/hyper-schema",
           "definitions" => root.resources.inject({}) { |result, (id, resource)|
