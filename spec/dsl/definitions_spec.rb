374c6359757135bcea1b393289733250bc7693ad
Pedro Belo <pedro@heroku.com>
2014-12-17 16:57:06 -0800

    test all types in definitions dsl

--- spec/dsl/definitions_spec.rb
+++ spec/dsl/definitions_spec.rb
@@ -5,22 +5,40 @@ describe Sinatra::Schema::DSL::Definitions do
   let(:dsl)      { described_class.new(resource, [resource.defs, resource.properties]) }
   let(:root)     { Sinatra::Schema::Root.instance }
 
-  it "adds a string definition to the resource" do
-    dsl.text(:foobar)
-    assert_equal 1, resource.defs.size
-    assert_equal "string", resource.defs[:foobar].type
-  end
-
-  it "adds a boolean definition to the resource" do
+  it "adds boolean definition to the resource" do
     dsl.bool(:foobar)
     assert_equal 1, resource.defs.size
     assert_equal "boolean", resource.defs[:foobar].type
   end
 
-  it "adds a datetime definition to the resource" do
+  it "adds datetime definition to the resource" do
     dsl.datetime(:foobar)
     assert_equal 1, resource.defs.size
     assert_equal "datetime", resource.defs[:foobar].type
+  end
+
+  it "adds email definition to the resource" do
+    dsl.email(:foobar)
+    assert_equal 1, resource.defs.size
+    assert_equal "email", resource.defs[:foobar].type
+  end
+
+  it "adds references" do
+    dsl.ref(:another_property)
+    assert_equal 1, resource.defs.size
+    assert_instance_of Sinatra::Schema::Reference, resource.defs[:another_property]
+  end
+
+  it "adds text definition to the resource" do
+    dsl.text(:foobar)
+    assert_equal 1, resource.defs.size
+    assert_equal "string", resource.defs[:foobar].type
+  end
+
+  it "adds uuid definition to the resource" do
+    dsl.uuid(:foobar)
+    assert_equal 1, resource.defs.size
+    assert_equal "uuid", resource.defs[:foobar].type
   end
 
   it "adds nested definitions" do
@@ -29,12 +47,6 @@ describe Sinatra::Schema::DSL::Definitions do
     assert_equal 2, resource.defs[:user].size
     assert_equal "string", resource.defs[:user][:email].type
     assert_equal "boolean", resource.defs[:user][:admin].type
-  end
-
-  it "adds references" do
-    dsl.ref(:another_property)
-    assert_equal 1, resource.defs.size
-    assert_instance_of Sinatra::Schema::Reference, resource.defs[:another_property]
   end
 
   it "sets other options" do
